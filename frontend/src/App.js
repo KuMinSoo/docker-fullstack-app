fb3465c5193a087783cd834de368b889a4e5ff65
minsu <minsu8012@naver.com>
2023-04-05 02:55:54 +0900

    ff

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,6 @@ import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
-axios.defaults.baseURL = '';
-
 function App() {
 
   useEffect(() => {
@@ -12,7 +10,7 @@ function App() {
       .then(response => {
         console.log('response', response)
       })
-  }, []);
+  }, [])
 
   useEffect(() => {
     //여기서 데이터베이스에 있는 값을 가져온다.
@@ -21,11 +19,11 @@ function App() {
         console.log('response', response)
         setLists(response.data)
       })
-  }, []);
+  }, [])
 
 
-  const [lists, setLists] = useState([]);
-  const [value, setValue] = useState("");
+  const [lists, setLists] = useState([])
+  const [value, setValue] = useState("")
 
 
   const changeHandler = (event) => {
@@ -38,11 +36,11 @@ function App() {
     axios.post('/api/value', { value: value })
       .then(response => {
         if (response.data.success) {
-          console.log('response', response);
-          setLists([...lists, response.data]);
+          console.log('response', response)
+          setLists([...lists, response.data])
           setValue("");
         } else {
-          alert('값을 DB에 넣는데 실패했습니다.');
+          alert('값을 DB에 넣는데 실패했습니다.')
         }
       })
   }
@@ -58,7 +56,7 @@ function App() {
             <li key={index}>{list.value} </li>
           ))}
           <br />
-          learn react
+            안녕하세요.
           <form className="example" onSubmit={submitHandler}>
             <input
               type="text"
